0e4251fff39c0b13cc2e1d72c705791179759794
Brian Richie Sr <brianrichiesr@gmail.com>
2023-10-04 08:38:24 -0700

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+const EyesOnMe = () => {
+    const printMessage = (str) => {
+        str === 'focus' ? console.log("Good!") :
+            console.log("Hey! Eyes on me!")
+    } 
+    return (
+        <button
+            onFocus={() => printMessage("focus")}
+            onBlur={printMessage}
+        >
+            Eyes on me
+        </button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
-// Code Keypad Component Here
-
-function Keypad (){
+const Keypad = () => {
+    const enterPassword = () => {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                onChange={() => enterPassword()}
+            />
+        </div>
     )
 }
 
